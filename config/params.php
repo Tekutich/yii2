0e087ae6dc7d8c6bde1ade8f670c1dc095287838
Tekutich <artdragon946@gmail.com>
2021-04-13 17:30:39 +0300

    Исправление ошибок, улучшение PHPdoc, мелкие правки

--- config/params.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'adminEmail' => 'admin@example.com',
-    'senderEmail' => 'noreply@example.com',
-    'senderName' => 'Example.com mailer',
-    'icon-framework' => \kartik\icons\Icon::FAS,  // Font Awesome Icon framework
-];
